a32c5e9ab0638566f4867cd4780d3b090085be66
CANHTN1 <trinhngoccanh97@gmail.com>
2025-07-06 22:00:03 +0700

    lan 2

--- PYTHON/bai2_input.py
+++ PYTHON/bai2_input.py
@@ -5,10 +5,7 @@ age = int(input("ban bao nhieu tuoi: "))
 print('cach1: chao ban %s,ban %d tuoi ha?, nam sau ban %d tuoi'%(name, age, age +1))
 print('cach2: chao ban {0},ban {1} tuoi ha ?'.format(name,age))
 print('')
-print('canh')
-print('canh1')
-print('canh2')
-print('canh2')
+
 
 #tinh dien tich va chu vi hinh chu nhat
 # nhap chieu dai chieu rong tu banphim
